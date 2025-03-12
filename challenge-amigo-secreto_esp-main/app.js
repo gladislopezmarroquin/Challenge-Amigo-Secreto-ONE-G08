5dada6b76e2fa4ae938eb716db69cb27d06a9d28
Gladis LM <gladislmarroquin@gmail.com>
2025-03-11 21:34:11 -0600

    Version final

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Amigo Secreto
+let amigos = [];  //Se hace un Array para almacenar la lista de amigos
+
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo"); // Obtiene el dato del input - Capturar el valor del campo de entrada
+    let nombre = input.value
+                        .trim() // Eliminar espacios en blanco al inicio y final 
+                        .replace(/\b\w/g, (amigo) => amigo.toUpperCase()); // Convierte la inicial mayuscula, ya que son nombres propios
+
+    if (nombre !== "") {
+        
+        amigos.push(nombre); // Agregar al array, el push sube o agrega al array lo ingresado desde el imput - Actualizar el array de amigos
+
+        actualizarListaDeAmigos(); // Actualizar la lista en el DOM
+        input.value = ""; // Limpiar el input
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
+           
+
+function actualizarListaDeAmigos() {
+    let lista = document.getElementById("listaAmigos"); // Obtiene el dato del input - Capturar el valor del campo de entrada
+   
+    lista.innerHTML = ""; // Limpiar la lista
+    
+    for (let i = 0; i < amigos.length; i++) {               // se crea un bucle que recorre toda la lista de amigos
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = amigos[i];
+        lista.appendChild(nuevoElemento);
+    }
+}
+
+
+function sortearAmigo() {
+    if (amigos.length >0){
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+        let amigoSeleccionado = amigos[indiceAleatorio];
+        let resultadoElemento = document.getElementById("resultado");
+        resultadoElemento.textContent = `El amigo secreto es: ${amigoSeleccionado}`;
+    } else {
+        alert("No hay amigos en la lista.");
+    }
+
+}
+    
+    
+
+
+
+
+
